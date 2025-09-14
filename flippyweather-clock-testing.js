e60cedd110c7255654cddfa0c5ad1827831cccf8
cnewman402 <cnewman402@gmail.com>
2025-09-14 15:42:24 -0500

    Update flippyweather-clock-testing.js

--- flippyweather-clock-testing.js
+++ flippyweather-clock-testing.js
@@ -7,12 +7,18 @@ import {
 } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
 class FlippyWeatherTesting extends LitElement {
+    setConfig(config) {
+        this._config = config;
+        console.log("Config set:", config);
+    }
+
     render() {
         return html`
             <ha-card>
                 <div style="padding: 20px; text-align: center;">
                     <h2>FlippyWeather Testing Loaded</h2>
                     <p>Element is working!</p>
+                    <p>Config: ${JSON.stringify(this._config)}</p>
                 </div>
             </ha-card>
         `;
